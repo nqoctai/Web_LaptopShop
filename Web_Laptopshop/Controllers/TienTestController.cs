2f1ca0194e7597a18c13e78482a4af4781a94e77
DevTapGym <2001224389@Huit.edu.vn>
2024-11-02 18:06:41 +0700

    TienTest

---
+++ Web_Laptopshop/Controllers/TienTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Laptopshop.Controllers
+{
+    public class TienTestController : Controller
+    {
+        //
+        // GET: /TienTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
