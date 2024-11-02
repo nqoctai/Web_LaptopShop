3b1e6832d8fff9ccea7019ce35b1adabeec574ec
Đặng Ngọc Tài <2001224231@hufi.edu.vn>
2024-11-02 18:23:50 +0700

    TaiTest

---
+++ Web_Laptopshop/Controllers/TaiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Laptopshop.Controllers
+{
+    public class TaiController : Controller
+    {
+        //
+        // GET: /Tai/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
